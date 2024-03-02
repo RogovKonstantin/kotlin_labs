17ded4365c155f23deef615e2667011cc5adbf82
kostya <d507756310@gmail.com>
2024-03-02 20:51:54 +0300

    m

--- src/main/kotlin/2_5.kt
+++ src/main/kotlin/2_5.kt
@@ -1,24 +1,24 @@
 package org.example
 
 
-    fun main() {
-        val num1: Int
-        val num2: Int
-        val num3: Int
+fun main() {
+    val num1: Int
+    val num2: Int
+    val num3: Int
 
-        try {
-            num1 = readLine()?.toInt() ?: 0
-            num2 = readLine()?.toInt() ?: 0
-            num3 = readLine()?.toInt() ?: 0
-        } catch (e: NumberFormatException) {
-            println("Ввод - целое число")
-            return
-        }
-
-        if (num1 == num2 || num1 == num3 || num2 == num3) {
-            println("Ошибка")
-        } else {
-            val average = (num1 + num2 + num3) / 3.0
-            println("Среднее значение: $average")
-        }
+    try {
+        num1 = readLine()?.toInt() ?: 0
+        num2 = readLine()?.toInt() ?: 0
+        num3 = readLine()?.toInt() ?: 0
+    } catch (e: NumberFormatException) {
+        println("Ввод - целое число")
+        return
     }
+    val numbers = listOf(num1, num2, num3)
+    val distinctNumbers = numbers.distinct().sorted()
+    if (distinctNumbers.size == 3) {
+        println(distinctNumbers[1])
+    } else {
+        println("Ошибка")
+    }
+}
