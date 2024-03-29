807fbbebeda71116283eff0733fa8f55976bbda0
kostya <d507756310@gmail.com>
2024-03-29 12:30:16 +0300

    m

--- src/main/kotlin/3_5.kt
+++ src/main/kotlin/3_5.kt
@@ -3,9 +3,9 @@ package org.example
 fun main() {
     var sum = 0
     for (i in 4..100 step 4) {
-        if (i % 4 == 0) {
+        //if (i % 4 == 0) {
             sum += i
-        }
+
     }
     println(sum)
 }
